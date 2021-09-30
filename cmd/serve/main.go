15757e3b4b7f421c28cd00b002aff6d38fb4df1a
Giau. Tran Minh <giau.tmg@gmail.com>
2021-09-30 11:44:23 +0000

    feat: added elk with sqllite db

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 
-	"viecco.dev/awesome/internal/hello"
+	elk "viecco.dev/awesome/ent/http"
+
+	"github.com/go-chi/chi/v5"
+	_ "github.com/mattn/go-sqlite3"
+	"go.uber.org/zap"
+	"viecco.dev/awesome/ent"
 )
 
 func main() {
-	fmt.Println(hello.Hello("World"))
+	// Create the ent client.
+	c, err := ent.Open("sqlite3", "./ent.db?_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer c.Close()
+	// Run the auto migration tool.
+	if err := c.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+	// Router and Logger.
+	r, l := chi.NewRouter(), zap.NewExample()
+	// Create the user handler.
+	r.Route("/v1", elk.NewUserHandler(c, l).MountRoutes)
+	// Start listen to incoming requests.
+	fmt.Println("Server running")
+	defer fmt.Println("Server stopped")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,18 @@ module viecco.dev/awesome
 
 go 1.17
 
-require entgo.io/ent v0.9.1
+require (
+	entgo.io/ent v0.9.2-0.20210822190728-d84ac191b1c7
+	github.com/go-chi/chi/v5 v5.0.4
+	github.com/mailru/easyjson v0.7.7
+	github.com/mattn/go-sqlite3 v1.14.8
+	go.uber.org/zap v1.19.1
+)
 
-require github.com/google/uuid v1.3.0 // indirect
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
