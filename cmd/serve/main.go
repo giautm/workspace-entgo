c422a2b982368060b1d26b8099cfd93cb97bff7d
Giau. Tran Minh <giau.tmg@gmail.com>
2021-10-01 09:48:36 +0000

    chore: added uber fx (Effect) for Dependency Injector
    For detail, see Godoc: https://pkg.go.dev/go.uber.org/fx

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -3,56 +3,239 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 
 	"github.com/getsentry/sentry-go"
 	sentryhttp "github.com/getsentry/sentry-go/http"
 	"github.com/go-chi/chi/v5"
 	_ "github.com/mattn/go-sqlite3"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
 	"go.uber.org/zap"
 	"viecco.dev/awesome/ent"
 	elk "viecco.dev/awesome/ent/http"
 )
 
-func main() {
-	err := sentry.Init(sentry.ClientOptions{
-		// Enable printing of SDK debug messages.
-		// Useful when getting started or trying to figure something out.
-		Debug: true,
-	})
+// NewLogger constructs a logger. It's just a regular Go function, without any
+// special relationship to Fx.
+//
+// Since it returns a *log.Logger, Fx will treat NewLogger as the constructor
+// function for the standard library's logger. (We'll see how to integrate
+// NewLogger into an Fx application in the main function.) Since NewLogger
+// doesn't have any parameters, Fx will infer that loggers don't depend on any
+// other types - we can create them from thin air.
+//
+// Fx calls constructors lazily, so NewLogger will only be called only if some
+// other function needs a logger. Once instantiated, the logger is cached and
+// reused - within the application, it's effectively a singleton.
+//
+// By default, Fx applications only allow one constructor for each type. See
+// the documentation of the In and Out types for ways around this restriction.
+func NewLogger() *zap.Logger {
+	logger := zap.NewExample()
+	logger.Info("Executing NewLogger.")
+	return logger
+}
+
+func NewSentry(logger *zap.Logger) (*sentryhttp.Handler, error) {
+	err := sentry.Init(sentry.ClientOptions{})
 	if err != nil {
-		log.Fatalf("sentry.Init: %s", err)
+		logger.Error(fmt.Sprintf("sentry.Init: %s", err))
+		return nil, err
 	}
 
+	return sentryhttp.New(sentryhttp.Options{}), nil
+}
+
+func NewEntClientFx(lc fx.Lifecycle, logger *zap.Logger) (*ent.Client, error) {
 	// Create the ent client.
-	c, err := ent.Open("sqlite3", "./ent.db?_fk=1")
+	client, err := ent.Open("sqlite3", "./ent.db?_fk=1")
 	if err != nil {
-		log.Fatalf("failed opening connection to sqlite: %v", err)
+		logger.Error(fmt.Sprintf("failed opening connection to sqlite: %v", err))
+		return nil, err
 	}
-	defer c.Close()
 
 	// Run the auto migration tool.
-	if err := c.Schema.Create(context.Background()); err != nil {
-		log.Fatalf("failed creating schema resources: %v", err)
+	if err := client.Schema.Create(context.Background()); err != nil {
+		logger.Error(fmt.Sprintf("failed creating schema resources: %v", err))
+		return nil, err
 	}
 
-	// Create an instance of sentryhttp
-	sentryHandler := sentryhttp.New(sentryhttp.Options{})
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			logger.Info("Close Ent client.")
+			return client.Close()
+		},
+	})
 
-	// Router and Logger.
+	return client, nil
+}
+
+// NewHandler constructs a simple HTTP handler. Since it returns an
+// http.Handler, Fx will treat NewHandler as the constructor for the
+// http.Handler type.
+//
+// Like many Go functions, NewHandler also returns an error. If the error is
+// non-nil, Go convention tells the caller to assume that NewHandler failed
+// and the other returned values aren't safe to use. Fx understands this
+// idiom, and assumes that any function whose last return value is an error
+// follows this convention.
+//
+// Unlike NewLogger, NewHandler has formal parameters. Fx will interpret these
+// parameters as dependencies: in order to construct an HTTP handler,
+// NewHandler needs a logger. If the application has access to a *log.Logger
+// constructor (like NewLogger above), it will use that constructor or its
+// cached output and supply a logger to NewHandler. If the application doesn't
+// know how to construct a logger and needs an HTTP handler, it will fail to
+// start.
+//
+// Functions may also return multiple objects. For example, we could combine
+// NewHandler and NewLogger into a single function:
+//
+//   func NewHandlerAndLogger() (*log.Logger, http.Handler, error)
+//
+// Fx also understands this idiom, and would treat NewHandlerAndLogger as the
+// constructor for both the *log.Logger and http.Handler types. Just like
+// constructors for a single type, NewHandlerAndLogger would be called at most
+// once, and both the handler and the logger would be cached and reused as
+// necessary.
+func NewHandler(sentryHandler *sentryhttp.Handler, c *ent.Client, logger *zap.Logger) (http.Handler, error) {
+	logger.Info("Executing NewHandler.")
+
 	r := chi.NewRouter().With(sentryHandler.Handle)
-	l := zap.NewExample()
 
-	// Create the user handler.
-	r.Route("/v1", elk.NewUserHandler(c, l).MountRoutes)
+	r.Route("/v1", elk.NewUserHandler(c, logger).MountRoutes)
 
-	// Start listen to incoming requests.
-	fmt.Println("Server running")
-	defer fmt.Println("Server stopped")
+	return r, nil
+}
 
-	sentry.CaptureMessage("It works!")
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		log.Fatal(err)
+// NewMux constructs an HTTP mux. Like NewHandler, it depends on *log.Logger.
+// However, it also depends on the Fx-specific Lifecycle interface.
+//
+// A Lifecycle is available in every Fx application. It lets objects hook into
+// the application's start and stop phases. In a non-Fx application, the main
+// function often includes blocks like this:
+//
+//   srv, err := NewServer() // some long-running network server
+//   if err != nil {
+//     log.Fatalf("failed to construct server: %v", err)
+//   }
+//   // Construct other objects as necessary.
+//   go srv.Start()
+//   defer srv.Stop()
+//
+// In this example, the programmer explicitly constructs a bunch of objects,
+// crashing the program if any of the constructors encounter unrecoverable
+// errors. Once all the objects are constructed, we start any background
+// goroutines and defer cleanup functions.
+//
+// Fx removes the manual object construction with dependency injection. It
+// replaces the inline goroutine spawning and deferred cleanups with the
+// Lifecycle type.
+//
+// Here, NewMux makes an HTTP mux available to other functions. Since
+// constructors are called lazily, we know that NewMux won't be called unless
+// some other function wants to register a handler. This makes it easy to use
+// Fx's Lifecycle to start an HTTP server only if we have handlers registered.
+func NewMux(lc fx.Lifecycle, logger *zap.Logger) *http.ServeMux {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
+
+	logger.Info("Executing NewMux")
+	// First, we construct the mux and server. We don't want to start the server
+	// until all handlers are registered.
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	// If NewMux is called, we know that another function is using the mux. In
+	// that case, we'll use the Lifecycle type to register a Hook that starts
+	// and stops our HTTP server.
+	//
+	// Hooks are executed in dependency order. At startup, NewLogger's hooks run
+	// before NewMux's. On shutdown, the order is reversed.
+	//
+	// Returning an error from OnStart hooks interrupts application startup. Fx
+	// immediately runs the OnStop portions of any successfully-executed OnStart
+	// hooks (so that types which started cleanly can also shut down cleanly),
+	// then exits.
+	//
+	// Returning an error from OnStop hooks logs a warning, but Fx continues to
+	// run the remaining hooks.
+	lc.Append(fx.Hook{
+		// To mitigate the impact of deadlocks in application startup and
+		// shutdown, Fx imposes a time limit on OnStart and OnStop hooks. By
+		// default, hooks have a total of 15 seconds to complete. Timeouts are
+		// passed via Go's usual context.Context.
+		OnStart: func(context.Context) error {
+			logger.Info("Starting HTTP server.", zap.String("port", port))
+			// In production, we'd want to separate the Listen and Serve phases for
+			// better error-handling.
+			go server.ListenAndServe()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			logger.Info("Stopping HTTP server.")
+			return server.Shutdown(ctx)
+		},
+	})
+
+	return mux
+}
+
+// Register mounts our HTTP handler on the mux.
+//
+// Register is a typical top-level application function: it takes a generic
+// type like ServeMux, which typically comes from a third-party library, and
+// introduces it to a type that contains our application logic. In this case,
+// that introduction consists of registering an HTTP handler. Other typical
+// examples include registering RPC procedures and starting queue consumers.
+//
+// Fx calls these functions invocations, and they're treated differently from
+// the constructor functions above. Their arguments are still supplied via
+// dependency injection and they may still return an error to indicate
+// failure, but any other return values are ignored.
+//
+// Unlike constructors, invocations are called eagerly. See the main function
+// below for details.
+func Register(mux *http.ServeMux, h http.Handler) {
+	mux.Handle("/", h)
+}
+
+func main() {
+	app := fx.New(
+		// Provide all the constructors we need, which teaches Fx how we'd like to
+		// construct the *log.Logger, http.Handler, and *http.ServeMux types.
+		// Remember that constructors are called lazily, so this block doesn't do
+		// much on its own.
+		fx.Provide(
+			NewLogger,
+			NewSentry,
+			NewEntClientFx,
+			NewHandler,
+			NewMux,
+		),
+		// Since constructors are called lazily, we need some invocations to
+		// kick-start our application. In this case, we'll use Register. Since it
+		// depends on an http.Handler and *http.ServeMux, calling it requires Fx
+		// to build those types using the constructors above. Since we call
+		// NewMux, we also register Lifecycle hooks to start and stop an HTTP
+		// server.
+		fx.Invoke(Register),
+
+		// This is optional. With this, you can control where Fx logs
+		// its events. In this case, we're using a NopLogger to keep
+		// our test silent. Normally, you'll want to use an
+		// fxevent.ZapLogger or an fxevent.ConsoleLogger.
+		fx.WithLogger(func(logger *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: logger}
+		}),
+
+		fx.StartTimeout(fx.DefaultTimeout),
+	)
+	app.Run()
 }
--- go.mod
+++ go.mod
@@ -4,17 +4,20 @@ go 1.17
 
 require (
 	entgo.io/ent v0.9.2-0.20210822190728-d84ac191b1c7
+	github.com/getsentry/sentry-go v0.11.0
 	github.com/go-chi/chi/v5 v5.0.4
 	github.com/mailru/easyjson v0.7.7
 	github.com/mattn/go-sqlite3 v1.14.8
+	go.uber.org/fx v1.14.2
 	go.uber.org/zap v1.19.1
 )
 
 require (
-	github.com/getsentry/sentry-go v0.11.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/dig v1.12.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
